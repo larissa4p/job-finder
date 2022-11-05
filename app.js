f4b245fdf18026f495e439d681de4d09e72b73b2
Larissa Bessa <98964013+larissa4p@users.noreply.github.com>
2022-11-05 14:05:05 -0300

    criando handlebars

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const app = express();
-const path = require('path');
+const path = require("path");
 const db = require("./db/connection");
 const bodyParser = require("body-parser");
 
@@ -14,10 +14,14 @@ app.listen(PORT, function () {
 // body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
 // handle bars
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+app.set("views", path.join(__dirname, "views"));
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 // db connection
 db.authenticate()
@@ -30,7 +34,7 @@ db.authenticate()
 
 // routes
 app.get("/", (req, res) => {
-  res.send("Está funcionando 3");
+  res.render("index");
 });
 
 // jobs routes
