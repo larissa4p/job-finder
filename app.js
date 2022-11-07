95d9f8479e63d9bcb641caaf4a4d730ecf90b4c3
Larissa Bessa <98964013+larissa4p@users.noreply.github.com>
2022-11-07 17:37:22 -0300

    Logica search, banco de dados, atualizando a view

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require("path");
 const db = require("./db/connection");
 const bodyParser = require("body-parser");
 const Job = require("./models/Job");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
 const PORT = 3000;
 
@@ -34,14 +36,40 @@ db.authenticate()
 
 // routes
 app.get("/", (req, res) => {
-  Job.findAll({ order: [["createdAt", "DESC"]] })
-  .then(jobs => {
+  let search = req.query.job;
+  let query = "%" + search + "%"; // PH -> PHP, Word -> wordpress, press -> Wordpress
 
-    res.render("index", {
-      jobs
-    });
-  })
+  if(!search) {
+    Job.findAll({order: [
+      ['createdAt', 'DESC']
+    ]})
+    .then(jobs => {
+  
+      res.render('index', {
+        jobs
+      });
+  
+    })
+    .catch(err => console.log(err));
+  } else {
+    Job.findAll({
+      where: {title: {[Op.like]: query}},
+      order: [
+        ['createdAt', 'DESC']
+    ]})
+    .then(jobs => {
+      console.log(search);
+      console.log(search);
+  
+      res.render('index', {
+        jobs, search
+      });
+  
+    })
+    .catch(err => console.log(err));
+  }
 
+  
 });
 
 // jobs routes
